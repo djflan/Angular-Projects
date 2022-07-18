c6466ef9c27bb032b634cef55dad003191965c8c
Dan Flanigan <djflan@gmail.com>
2022-07-18 16:19:56 -0500

    Current status on tour of heroes

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,15 +10,19 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
-  
-  hero: Hero = {
-    id:1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
 
-  constructor() { }
+  constructor() 
+  { 
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+  }
+
+  onSelect(hero: Hero): void 
+  {
+    this.selectedHero = hero;
   }
 
 }
